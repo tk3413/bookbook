007d1931c766c2ae7b835827216cd0d76e93f554
Taimore Khan <khan.taimore@gmail.com>
2022-09-22 22:33:00 -0400

    fixing formatting

--- src/main/java/com/taimorekhan/bookbook/BookbookApplication.java
+++ src/main/java/com/taimorekhan/bookbook/BookbookApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookbookApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BookbookApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+	SpringApplication.run(BookbookApplication.class, args);
+    }
 }
