2f5b84dae5909a61e6e81c4a1ef9ff01a62308cc
Taimore Khan <khan.taimore@gmail.com>
2022-09-22 22:07:23 -0400

    added a canary test

--- src/main/java/com/taimorekhan/bookbook/models/Library.java
+++ src/main/java/com/taimorekhan/bookbook/models/Library.java
@@ -2,14 +2,12 @@ package com.taimorekhan.bookbook.models;
 
 import java.util.UUID;
 import java.util.List;
-import com.taimorekhan.bookbook.models.Author;
 import lombok.Data;
 
 @Data
 public class Library {
     UUID id;
     String title;
-    Author author;
     String type;
     String status;
     List<String> categories;
---
+++ src/test/java/com/taimorekhan/bookbook/controllers/AuthorControllerTest.java
@@ -0,0 +1,12 @@
+package com.taimorekhan.bookbook.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthorControllerTest {
+    @Test
+	public void canaryTest() {
+		assertEquals(1, 1);
+	}
+}
