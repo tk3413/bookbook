5a9f1391d4019d8aaf4eaa38d21fa10661a55ab6
Taimore Khan <khan.taimore@gmail.com>
2022-10-14 00:31:57 -0400

    added repository skeleton

--- src/main/java/com/taimorekhan/bookbook/repositories/AuthorRepository.java
+++ src/main/java/com/taimorekhan/bookbook/repositories/AuthorRepository.java
@@ -2,6 +2,14 @@ package com.taimorekhan.bookbook.repositories;
 
 import org.springframework.stereotype.Repository;
 
+import com.taimorekhan.bookbook.models.Author;
+
 
 @Repository
-public class AuthorRepository {}
+public class AuthorRepository implements AuthorRepositoryInterface {
+
+    @Override
+    public Author getAuthor() {
+        // TODO Auto-generated method stub
+        return null;
+    }}
--- src/test/java/com/taimorekhan/bookbook/repositories/AuthorRepositoryTest.java
+++ src/test/java/com/taimorekhan/bookbook/repositories/AuthorRepositoryTest.java
@@ -1,20 +1,26 @@
 package com.taimorekhan.bookbook.repositories;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.taimorekhan.bookbook.models.Author;
 
+@RunWith( SpringRunner.class )
+@SpringBootTest
 public class AuthorRepositoryTest {
 
     @Autowired
     AuthorRepository authorRepository;
 
-    public void wip() {
+    @Test
+    public void testGetAuthor() {
         Author testAuthor = Author.builder()
                 .firstName("taimore")
                 .lastName("khan")
                 .build();
-        // assert (testAuthor == authorRepository.getAuthor());
+        assert (null == authorRepository.getAuthor());
     }
 }
